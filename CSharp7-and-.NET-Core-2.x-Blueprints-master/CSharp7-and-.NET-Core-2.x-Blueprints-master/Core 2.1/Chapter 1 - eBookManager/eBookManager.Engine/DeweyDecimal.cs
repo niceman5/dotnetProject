a077e3d2db201aa3865dab4ffec5189d399478fa
DESKTOP-N3RMADH\김태훈 <niceman555@gmail.com>
2020-08-04 18:11:29 +0900

    예제 먼저 기록함

---
+++ CSharp7-and-.NET-Core-2.x-Blueprints-master/CSharp7-and-.NET-Core-2.x-Blueprints-master/Core 2.1/Chapter 1 - eBookManager/eBookManager.Engine/DeweyDecimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookManager.Engine
+{
+    public class DeweyDecimal
+    {
+        public string ComputerScience { get; set; } = "000";
+        public string DataProcessing { get; set; } = "004";
+        public string ComputerProgramming { get; set; } = "005";
+    }
+}
---
+++ CSharp7-and-.NET-Core-2.x-Blueprints-master/CSharp7-and-.NET-Core-2.x-Blueprints-master/Core 2.1/Chapter 1 - eBookManager/eBookManager.Engine/Document.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookManager.Engine
+{
+    public class Document
+    {
+        public string Title { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public DateTime LastAccessed { get; set; }
+        public DateTime Created { get; set; }
+        public string FilePath { get; set; }
+        public string FileSize { get; set; }
+        public string ISBN { get; set; }
+        public string Price { get; set; }
+        public string Publisher { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DeweyDecimal Classification { get; set; }
+        public string Category { get; set; }
+    }
+}
+
